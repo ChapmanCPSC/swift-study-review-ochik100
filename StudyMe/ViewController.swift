ed5d4f3b09af9834eafef56474e88eb26731cd1d
Kelly Ochikubo <ochik100@mail.chapman.edu>
2016-03-29 16:44:40 -0700

    Initial Commit

---
+++ StudyMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudyMe
+//
+//  Created by Kelly Ochikubo on 3/29/16.
+//  Copyright © 2016 Kelly Ochikubo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
